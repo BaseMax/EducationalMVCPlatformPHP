bd185450112dc25f893bf219d6252585d070c613
aliiahmadi <aliahmadi.github@gmail.com>
2023-05-16 13:59:03 +0330

    working in Router class

---
+++ App/Router/Router.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Application\Router;
+
+class Router implements RouterInterface
+{
+    /**
+     * returns an array of route from routing table
+     * @var array
+     */
+    protected array $routes = [];
+
+    /**
+     * returns an array of route parameters
+     * @var array
+     */
+    protected array $params = [];
+
+    /**
+     * Adds a suffix onto the controller name
+     * @var string
+     */
+    protected string $controllerSuffix = "controller";
+
+
+    /**
+     * @inheritDoc
+     */
+    public function add(string $route, array $params = []): void
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function dispatch(string $url): void
+    {
+        if ($this->match($url)) {
+            $controllerString = $this->params["controller"];
+            $controllerString = $this->transformUpperCamelCase($controllerString);
+            $controllerString = $this->getNamespace($controllerString);
+        }
+    }
+
+    /**
+     * match the route to the routes in the routing table and
+     * setting the $this->params property if a route is found
+     * 
+     * @param string $url
+     * @return bool
+     */
+    private function match(string $url): bool
+    {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $param) {
+                    if (is_string($key)) {
+                        $params[$key] = $param;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public function transformUpperCamelCase(string $string): string
+    {
+        return str_replace(" ", "", ucwords(str_replace("-", " ", $string)));
+    }
+
+
+    /**
+     * Get the namespace for the controller class.
+     * the namespace defined within the route parameters
+     * only if it was added.
+     * 
+     * @param string $string
+     * @return string
+     */
+    public function getNamespace(string $string): string
+    {
+        $namespace = "Application\Controller\\";
+        if (array_key_exists("namespace", $this->params)) {
+            $namespace .= $this->params["namespace"] . "\\";
+        }
+
+        return $namespace;
+    }
+}
---
+++ App/Router/RouterInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Application\Router;
+
+interface RouterInterface
+{
+    /**
+     * Simple add a route to routing table
+     * 
+     * @param string $route
+     * @param array $params
+     * @return void 
+     */
+    public function add(string $route, array $params): void;
+
+    /**
+     * Dispatch route and create controller objects and execute the default method
+     * on that controller object
+     * 
+     * @param string $url
+     * @return void
+     */
+    public function dispatch(string $url): void;
+}
