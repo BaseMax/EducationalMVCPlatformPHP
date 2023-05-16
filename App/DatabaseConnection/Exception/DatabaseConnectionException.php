070f5891d43877700468f8fa533ad87b1babe179
aliiahmadi <aliahmadi.github@gmail.com>
2023-05-16 20:02:16 +0330

    Database access layer #1

--- App/DatabaseConnection/Exception/DatabaseConnectionException.php
+++ App/DatabaseConnection/Exception/DatabaseConnectionException.php
@@ -6,11 +6,15 @@ use PDOException;
 
 class DatabaseConnectionException extends PDOException
 {
-    protected $message;
-    protected $code;
-
-
-    public function __construct($message = null, $code = null)
+    /**
+     * Main constructor class which overrides the parent constructor 
+     * and set the message and the code properties which is optional
+     * 
+     * @param string $message
+     * @param int $code
+     * @return void
+     */
+    public function __construct(string $message = null, int $code = null)
     {
         $this->message = $message;
         $this->code = $code;
---
+++ App/ORM/DataMapper/Exception/DataMapperException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Application\ORM\DataMapper\Exception;
+
+use Exception;
+
+class DataMapperException extends Exception
+{
+}
