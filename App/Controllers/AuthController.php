9aa2f960a79b196f19c054df83bdab24fcd49aaa
aliiahmadi <aliahmadi.github@gmail.com>
2023-05-26 20:02:01 +0330

    login

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace Application\Controllers;
 
 use Application\Exceptions\ValidationError;
+use Application\Facades\Hash;
 use Application\Facades\JWT;
 use Application\Facades\Request;
 use Application\Facades\Response;
@@ -14,19 +15,11 @@ class AuthController extends Controller
 {
     public function register()
     {
-        $userData = Request::post();
-
-        $validation = (new Validator())->make($userData, [
-            "name" => "required|min:5",
-            "email" => "required|email",
+        $userData = $this->validate(Request::post(), [
+            "name"     => "required|min:5",
+            "email"    => "required|email",
             "password" => "required|min:6",
         ]);
-
-        $validation->validate();
-
-        if ($validation->fails()) {
-            ValidationError::error($validation->errors()->firstOfAll());
-        }
 
         $user = (new User())
             ->setName($userData["name"])
@@ -51,5 +44,27 @@ class AuthController extends Controller
 
     public function login()
     {
+        $userData = $this->validate(Request::post(), [
+            "email"    => "required|email",
+            "password" => "required|min:6"
+        ]);
+
+        $entityManager = (new User())->getEntityManager();
+
+        $user = $entityManager->getRepository(User::class)->findOneBy([
+            "email"    => $userData["email"],
+        ]);
+
+        if (!$user) ValidationError::error(["you are not registered yet."]);
+
+        if (!Hash::verify($userData["password"], $user->getPassword())) ValidationError::error(["your password is incorrect."]);
+
+        return Response::json([
+            "token" => JWT::encode([
+                $user->getName(),
+                $user->getEmail(),
+                $user->getPassword()
+            ])
+        ]);
     }
 }
--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -2,6 +2,19 @@
 
 namespace Application\Controllers;
 
+use Application\Exceptions\ValidationError;
+use Rakit\Validation\Validator;
+
 class Controller
 {
+    public function validate(array $userData, array $rules)
+    {
+        $validation = (new Validator())->make($userData, $rules);
+
+        $validation->validate();
+
+        if ($validation->fails()) ValidationError::error($validation->errors()->firstOfAll());
+
+        return $userData;
+    }
 }
