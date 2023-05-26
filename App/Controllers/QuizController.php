f3d7530dd0c06b890d40569976fabf44bcb08da9
aliiahmadi <aliahmadi.github@gmail.com>
2023-05-26 23:10:16 +0330

    quiz controller

--- App/Controllers/QuizController.php
+++ App/Controllers/QuizController.php
@@ -2,30 +2,96 @@
 
 namespace Application\Controllers;
 
+use Application\Facades\Request;
+use Application\Facades\Response;
+use Application\Models\Content;
+
 class QuizController extends Controller
 {
     public function index()
     {
         $this->auth();
+
+        $quiezzes = (new Content())
+            ->builder()
+            ->select("*")
+            ->from("quizzes")
+            ->fetchAllAssociative();
+
+        return Response::json($quiezzes);
     }
 
-    public function show()
+    public function show($id)
     {
         $this->auth();
+
+        $quiz = (new Content())
+            ->builder()
+            ->select("*")
+            ->from("quizzes")
+            ->where("id = ?")
+            ->setParameter(0, $id)
+            ->fetchAssociative();
+
+        return Response::json($quiz);
     }
 
     public function store()
     {
         $this->auth();
+
+        $userData = $this->validate(Request::post(), [
+            "content_id"  => "required|numeric",
+            "title"       => "required"
+        ]);
+
+        (new Content())
+            ->builder()
+            ->insert("quizzes")
+            ->values([
+                "content_id" => $userData["content_id"],
+                "title"      => $userData["title"]
+            ]);
+
+        return Response::json([
+            "detail" => "quiz created successfuly"
+        ]);
     }
 
-    public function update()
+    public function update($id)
     {
         $this->auth();
+
+        $userData = $this->validate(Request::post(), [
+            "content_id"  => "required|numeric",
+            "title"       => "required"
+        ]);
+
+        (new Content())
+            ->builder()
+            ->update("quizzes")
+            ->set("content_id", $userData["content_id"])
+            ->set("title", $userData["title"])
+            ->where("id = ?")
+            ->setParameter(0, $id);
+
+        return Response::json([
+            "detail" => "quiz updated successfuly."
+        ]);
     }
 
-    public function destroy()
+    public function destroy($id)
     {
         $this->auth();
+
+        (new Content())
+            ->builder()
+            ->delete("quizzes")
+            ->where("id = ?")
+            ->setParameter(0, $id);
+
+        return Response::json([
+            "detail" => "quiz deleted successfuly."
+        ]);
     }
 }
